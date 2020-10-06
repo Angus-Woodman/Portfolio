d492d3dcf0e3c342c3960c570c03f1cca942d7fc
Angus Woodman <angus.woodman@outlook.comgit config --global user.name yourGithubUsernameAngus-Woodman>
2020-10-06 18:10:31 +0100

    initial setup for project page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles/App.css";
 import NavBar from "./Components/NavBar/NavBar"
 import { Switch, Route } from "react-router-dom";
-import {Welcome, About} from "./Pages"
+import {Welcome, About, Projects} from "./Pages"
 
 class App extends React.Component {
   state = {};
@@ -14,7 +14,7 @@ class App extends React.Component {
             <Switch>
                <Route exact path="/" component={Welcome} />
                <Route path="/about" component={About} />
-               {/*<Route path="/projects" component={Projects} />*/}
+               <Route path="/projects" component={Projects} />
              </Switch>
             </main>
       </>
