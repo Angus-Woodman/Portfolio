51cecc68464822e17220ac3b8b9c52081f4c94f3
Angus Woodman <angus.woodman@outlook.comgit config --global user.name yourGithubUsernameAngus-Woodman>
2020-10-08 13:58:04 +0100

    adjust project image sizes

--- src/Components/ProjectSlideshow.js
+++ src/Components/ProjectSlideshow.js
@@ -25,26 +25,19 @@ const projects =  [
 {
   id:2,
   link: 'https://musing-hopper-288ea1.netlify.app/',
-  img: 'https://i.imgur.com/25viw5t.png',
+  img: 'https://i.imgur.com/cnE6IUu.png',
   title: 'Photography Blogging Project',
   description: 'We were tasked with building a blogging platform where users can anonymously post content of their choice and can comment and like other posts that have been made.'
 },
 {
   id:3,
   link: 'https://5f0703141d0b73150fb224a2--chelsea-project-angus.netlify.app',
-  img: 'https://i.imgur.com/EBsh1jN.png',
+  img: 'https://i.imgur.com/62TWH37.png',
   title: 'Chelsea Project',
   description: 'Personal project when learning about tables.'
 },
 {
   id:4,
-  link: 'https://5f07040e1d0b731b27b2249f--food-project-angus.netlify.app',
-  img: 'https://i.imgur.com/ZWOHpys.png',
-  title: 'Food Project',
-  description: 'Personal project when learning about ...'
-},
-{
-  id:5,
   link: 'https://angusannagooglesearch.netlify.app/',
   img: 'https://i.imgur.com/D0bgnD2.png',
   title: 'Google Project',
@@ -58,12 +51,11 @@ const ProjectSlideshow = () => {
         <Slide {...fadeProperties}>
           {projects.map((project) => (
             <div key={project.id} className="each-slide">
-              <a href={project.link}>
-                <div className="slideImage" style={{'backgroundImage': `url(${project.img})`}}>
-                  <span>{project.title}</span>
+              <a href={project.link} target="_blank">
+                <div className="slideImage" style={{'backgroundImage': `url(${project.img})`, 'backgroundRepeat':'no-repeat', 'backgroundSize':'cover'}}>
                 </div>
-                <div class="overlay">
-                  <div class="text">
+                <div className="overlay">
+                  <div className="text">
                     <h2>{project.title}</h2>
                     <p>{project.description}</p>
                   </div>
