b4b1df9d9d5c85b72e2690c1af7ac2d2a87252e9
Angus Woodman <angus.woodman@outlook.comgit config --global user.name yourGithubUsernameAngus-Woodman>
2020-10-09 11:49:03 +0100

    add in social media icons to welcome page

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -6,6 +6,10 @@ const Welcome = () => (
       <div className="welcomeContainer">
         <h1 className='welcomeTitle'>Angus Woodman</h1>
         <p>I'm a London based <strong>Full-Stack Web Developer</strong>.</p>
+        <hr width='60%'/>
+        <i className="fa fa-github fa-2x" aria-hidden="true"></i>
+        <i class="fa fa-linkedin fa-2x" aria-hidden="true"></i>
+        <i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
       </div>
     </>
 )
