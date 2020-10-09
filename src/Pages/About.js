ee5dbce9d2ccc7b943677dd73b6b4bd4b67d89b4
Angus Woodman <angus.woodman@outlook.comgit config --global user.name yourGithubUsernameAngus-Woodman>
2020-10-09 11:17:10 +0100

    add content and styling to about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import '../styles/About.css'
 
 const About = () => {
     return (
-        <div id="about" style={{ textAlign: "justify", padding: "5%" }}>
-            <h2>About Me</h2>
-            <p> When I was 16 I got involved in a Greenpower project, where we had to design and build a battery powered go-kart, which we raced at the Goodwood racetrack. This experience fueled my creative passion for technology, and led me down a path towards software development. My ability to continuously work and occasionally lose myself in code assures me that I have made the right decision in pursuing a career in programming. Outside of programming I am a keen sportsman playing lots of golf, cricket, football, tennis and squash and I am also an avid Chelsea fan.</p>
-
-            <p> Throughout my Engineering Mathematics degree I was involved in several projects, where we were tasked with constructing detailed mathematical models, using programming languages Matlab and Python, to solve various real-world problems. This introduction to programming and the satisfaction I experienced from finishing each one of these projects fuelled my desire to pursue a career in programming. After finishing my degree I worked on a robotics project for a real estate consultancy. The company wanted to be able to automate the task of taking multiple photos of the inside of warehouses using a 360o camera. I was tasked with finding the best possible solution and building a prototype, which the team could run tests on. During this project I had to learn fast, dealing with unfamiliar pieces of hardware, such as a Pixhawk and a RasberryPi. The experience gave me valuable problem solving and research skills, which I believe will allow me to hit the ground running when I begin my job. Since then I have completed a Full Stack Web Developer course with Futureproof, which has given me a great base to further develop my programming and web development skills. </p>
+        <div id="about">
+            <div className='aboutSection'>
+                <img src="https://i.imgur.com/0ednlgt.png" alt="Angus Woodman Headshot" />
+                <h3>Contact details:</h3>
+                <p>+447375646432</p>
+                <p>angus.woodman@outlook.com</p>
+                <button>Download CV</button>
+            </div>
+            <div className='aboutSection'>
+                <h1>About Me</h1>
+                <p> I am an Engineering Mathematics graduate from the University of Bristol and have trained as a Full-Stack Web Developer with Futureproof.</p>
+                <h3>University</h3>
+                <p> Throughout my Engineering Mathematics degree I was involved in several projects, where we were tasked with constructing detailed mathematical models, using programming languages Matlab and Python, to solve various real-world problems. This introduction to programming and the satisfaction I experienced from finishing each one of these projects fuelled my desire to pursue a career in programming. </p>
+                <h3>Robotics Project</h3>
+                <p>After finishing my degree I worked on a robotics project for a real estate consultancy. The company wanted to be able to automate the task of taking multiple photos of the inside of warehouses using a 360o camera. I was tasked with finding the best possible solution and building a prototype, which the team could run tests on. During this project I had to learn fast, dealing with unfamiliar pieces of hardware, such as a Pixhawk and a RasberryPi.</p>
+                <h3>Futureproof</h3>
+                <p>Since then I have completed a Full Stack Web Developer course with Futureproof, which has given me a great base to further develop my programming and web development skills.</p>
+            </div>
         </div>
     );
 }
--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -6,7 +6,6 @@ const Welcome = () => (
       <div className="welcomeContainer">
         <h1 className='welcomeTitle'>Angus Woodman</h1>
         <p>I'm a London based <strong>Full-Stack Web Developer</strong>.</p>
-        <img src="https://i.imgur.com/0ednlgt.png" alt="Angus Woodman Headshot" />
       </div>
     </>
 )
