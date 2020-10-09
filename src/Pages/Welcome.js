82624968a966bc29d0b39fe99b2df5293014a747
Angus Woodman <angus.woodman@outlook.comgit config --global user.name yourGithubUsernameAngus-Woodman>
2020-10-09 12:34:36 +0100

    add links to social media icons

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import '../styles/Welcome.css'
 
 const Welcome = () => (
@@ -7,9 +8,9 @@ const Welcome = () => (
         <h1 className='welcomeTitle'>Angus Woodman</h1>
         <p>I'm a London based <strong>Full-Stack Web Developer</strong>.</p>
         <hr width='60%'/>
-        <i className="fa fa-github fa-2x" aria-hidden="true"></i>
-        <i class="fa fa-linkedin fa-2x" aria-hidden="true"></i>
-        <i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
+        <a href='https://github.com/Angus-Woodman/' target="_blank"> <i className="fa fa-github fa-2x" aria-hidden='True'></i> </a>
+        <a href='https://www.linkedin.com/in/angus-woodman-6a3072151/' target="_blank"> <i className="fa fa-linkedin fa-2x" aria-hidden='True'></i> </a>
+        <a href='https://www.facebook.com/woodpecker25/' target="_blank"> <i className="fa fa-facebook-official fa-2x" aria-hidden='True'></i> </a>
       </div>
     </>
 )
