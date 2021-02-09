82359b09ce647a2e70ec3cf866efc49d8946b4d6
Angus Woodman <>
2021-02-09 15:28:55 +0000

    small edits to about and project content and clean up spacing of cards in project page

--- src/Components/ProjectSlideshow.js
+++ src/Components/ProjectSlideshow.js
@@ -23,51 +23,72 @@ const projects =  [
     id:1,
     link: 'https://image-crud-test.netlify.app',
     img: 'https://i.imgur.com/cnE6IUu.png',
-    title: 'Photography Blogging Project',
-    description: 'Reworked first group project from the FutureProof course using a React front-end, deployed on netlify, express server, MongoDB Atlas database, cloundinary to store the images and back-end deployed on Heroku.'
+    title: 'Reworked Fotographiq project',
+    description: 'Reworked first group project from the FutureProof course: Fotographiq, a photography blogging platform for photography enthusiasts.',
+    frontEnd: 'React',
+    backEnd: 'JavaScript & Express Server, with a MongoDB database',
+    deployment: 'Front-end: Netlify, Back-end: Heroku and MongoDB Atlas, Cloudinary to store image files.'
   },
   {
   id:2,
   link: 'https://trivia-boss.netlify.app/',
   img: 'https://i.imgur.com/H38LDpI.png',
   title: 'Trivia Boss Project',
-  description: 'We were tasked with building a quiz application that you can play with your friends on a single application.'
+  description: 'First ever React project. We were tasked with building a quiz application that you can play with your friends on a single application. Utilizes the open source API, Open Trivia Database, for the questions. ',
+  frontEnd: 'React',
+  backEnd: 'N/A',
+  deployment: 'Netlify'
 },
 {
   id:3,
   link:'https://github.com/Angus-Woodman/captains-log',
   img: 'https://i.imgur.com/QUFDIab.png',
   title: 'Captain\'s Log - Football team management platform',
-  description: 'We were tasked with building a full stack web application with a python backend of our choice. Deployed site coming soon!!'
+  description: 'We were tasked with building a full stack web application with a python backend of our choice.',
+  frontEnd: '',
+  backEnd: '',
+  deployment: 'Deployed site coming soon'
 },
 {
   id:4,
   link: 'https://github.com/Angus-Woodman/Habitapp',
   img: 'https://i.imgur.com/X7YYqNJ.png',
   title: 'Habit tracking Project',
-  description: 'We were tasked with building a habbit tracker application.'
+  description: 'We were tasked with building a habbit tracker application.',
+  frontEnd: '',
+  backEnd: '',
+  deployment: 'Deployed site coming soon'
 },
 {
   id:5,
   link: 'https://musing-hopper-288ea1.netlify.app/',
   img: 'https://i.imgur.com/cnE6IUu.png',
   title: 'Photography Blogging Project',
-  description: 'We were tasked with building a blogging platform where users can anonymously post content of their choice and can comment and like other posts that have been made.'
+  description: 'We were tasked with building a blogging platform where users can anonymously post content of their choice and can comment and like other posts that have been made.',
+  frontEnd: '',
+  backEnd: '',
+  deployment: 'Deployed site coming soon'
 },
 {
   id:6,
-  link: 'https://5f0703141d0b73150fb224a2--chelsea-project-angus.netlify.app',
-  img: 'https://i.imgur.com/CI5ezLQ.png',
-  title: 'Chelsea Project',
-  description: 'Personal project when learning about tables.'
-},
-{
-  id:7,
   link: 'https://angusannagooglesearch.netlify.app/',
   img: 'https://i.imgur.com/RppZnsL.png',
   title: 'Google Project',
-  description: 'We were tasked with trying to recreate google. We chose to intergrate googles search API.'
-}
+  description: 'We were tasked with trying to recreate google. We chose to intergrate googles search API.',
+  frontEnd: '',
+  backEnd: 'N/A',
+  deployment: 'Netlify'
+},
+{
+  id:7,
+  link: 'https://5f0703141d0b73150fb224a2--chelsea-project-angus.netlify.app',
+  img: 'https://i.imgur.com/CI5ezLQ.png',
+  title: 'Chelsea Project',
+  description: 'Personal project at a very early stage in the Futureproof course.',
+  frontEnd: '',
+  backEnd: 'N/A',
+  deployment: 'Netlify'
+},
 ]
 
 const ProjectSlideshow = () => {
